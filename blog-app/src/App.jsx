1035de18d8e38da4b17e75bbec7642462d9c3901
giahienpham <giahien14604@gmail.com>
2024-07-11 14:15:52 +0700

    create and run server using express and nodemon

---
+++ blog-app/src/App.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import Dashboard from './pages/Dashboard'
+import Projects from './pages/Projects'
+import Signin from './pages/Signin'
+import Signup from './pages/Signup'
+import About from './pages/About'
+import Header from './components/Header'
+
+export default function App() {
+  return (
+    <BrowserRouter>
+    <Header/>
+      <Routes>
+        <Route path='/' element={<Home/>}></Route>
+        <Route path='/dashboard' element={<Dashboard/>}></Route>
+        <Route path='/projects' element={<Projects/>}></Route>
+        <Route path='/signin' element={<Signin/>}></Route>
+        <Route path='/signup' element={<Signup/>}></Route>
+        <Route path='/about' element={<About/>}></Route>
+      </Routes>
+    </BrowserRouter>
+  )
+}
