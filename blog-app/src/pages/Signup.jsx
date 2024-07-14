cc5ea697fb04596733b1033e1af1bb0a14d466bc
giahienpham <giahien14604@gmail.com>
2024-07-14 16:14:42 -0400

    create signup page UI

--- blog-app/src/pages/Signup.jsx
+++ blog-app/src/pages/Signup.jsx
@@ -1,9 +1,68 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+import { Button, Checkbox, Label, TextInput } from "flowbite-react";
 
 export default function Signup() {
   return (
-    <div>
-      Signup
+    <div className="min-h-screen mt-20">
+      <div className="flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-5">
+        <div className="flex-1">
+          <Link to="/" className="font-bold dark:text-white text-4xl">
+            <span className="px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white">
+              Hien's
+            </span>
+            Blog
+          </Link>
+          <p className="text-sm mt-5">
+            This is a demo project. You can sign up with your email and password
+            or with Google.
+          </p>
+        </div>
+        {/*right*/}
+        <div className="flex-1">
+          <form className="flex flex-col gap-1.5">
+            <div className="">
+              <Label value="Your username" />
+              <TextInput
+                type="text"
+                placeholder="Username"
+                id="username"
+                required
+              />
+            </div>
+            <div>
+              <Label htmlFor="email1" value="Your email" />
+              <TextInput
+                id="email1"
+                type="email"
+                placeholder="name@company.com"
+                required
+              />
+            </div>
+            <div>
+              <Label htmlFor="password1" value="Your password" />
+              <TextInput id="password1" type="password" required />
+            </div>
+            <div className="flex items-center gap-2 mt-2">
+              <Checkbox id="remember" />
+              <Label htmlFor="remember">Remember me</Label>
+            </div>
+            <Button
+              gradientDuoTone="purpleToBlue"
+              className="mt-2"
+              type="submit"
+            >
+              Sign Up
+            </Button>
+          </form>
+          <div className="flex gap-2 text-sm mt-5">
+            <span>Have an account?</span>
+            <Link to="/signin" className="text-blue-500">
+              Sign In
+            </Link>
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
