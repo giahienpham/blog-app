00664855349229d536190f5bce41226c5b75799e
giahienpham <giahien14604@gmail.com>
2024-07-20 11:16:41 -0400

    add signout functionality

--- blog-app/src/components/DashboardProfile.jsx
+++ blog-app/src/components/DashboardProfile.jsx
@@ -18,6 +18,7 @@ import {
   deleteUserStart,
   deleteUserSuccess,
   deleteUserFailure,
+  signoutSuccess,
 } from "../redux/user/userSlice";
 import { useDispatch } from "react-redux";
 
@@ -155,6 +156,23 @@ export default function DashboardProfile() {
       dispatch(deleteUserFailure(error.message));
     }
   };
+
+  // handling signout
+  const handleSignout = async () => {
+    try {
+      const res = await fetch('/api/user/signout', {
+        method: 'POST',
+      });
+      const data = await res.json();
+      if (!res.ok) {
+        console.log(data.message);
+      } else {
+        dispatch(signoutSuccess());
+      }
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
   return (
     <div className="max-w-lg mx-auto p-3 w-full">
       <h1 className="my-7 text-center font-semibold text-3xl">Profile</h1>
@@ -232,7 +250,9 @@ export default function DashboardProfile() {
         <span onClick={() => setShowModal(true)} className="cursor-pointer">
           Delete Account
         </span>
-        <span className="cursor-pointer">Sign Out</span>
+        <span onClick={handleSignout} className='cursor-pointer'>
+          Sign Out
+        </span>
       </div>
       {/* Alert for uploading */}
       {updateUserSuccess && (
@@ -250,7 +270,7 @@ export default function DashboardProfile() {
           {error}
         </Alert>
       )}
-      
+
       {/* Delete user */}
       <Modal
         show={showModal}
