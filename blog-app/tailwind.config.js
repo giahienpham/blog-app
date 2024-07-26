9ca64ba1c112fc0b89c265f00864bc2b014ab7d1
giahienpham <giahien14604@gmail.com>
2024-07-26 10:02:45 -0400

    add all userpost to dashboard

--- blog-app/tailwind.config.js
+++ blog-app/tailwind.config.js
@@ -2,9 +2,9 @@ const flowbite = require("flowbite-react/tailwind");
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js,ts,tsx,jsx}",  flowbite.content()],
+  content: ["./src/**/*.{html,js,ts,tsx,jsx}", flowbite.content()],
   theme: {
     extend: {},
   },
-  plugins: [flowbite.plugin()],
-}
+  plugins: [flowbite.plugin(), require("tailwind-scrollbar")],
+};
