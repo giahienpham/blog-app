344dc6cc2d194813af739af7cfd3175af6014a9c
giahienpham <giahien14604@gmail.com>
2024-07-26 11:49:23 -0400

    add showmore button

--- blog-app/src/pages/CreatePost.jsx
+++ blog-app/src/pages/CreatePost.jsx
@@ -104,7 +104,7 @@ export default function CreatePost() {
             }
           >
             <option value="uncategorized">Select a category</option>
-            <option value="javascript">JavaScript</option>
+            <option value="javascript">About us</option>
             <option value="reactjs">React.js</option>
             <option value="nextjs">Next.js</option>
           </Select>
