5774ddfa88ff7280adce459dd96c2c54129bc563
giahienpham <giahien14604@gmail.com>
2024-07-28 10:02:32 -0400

    create edit post functionality

--- api/routes/post.route.js
+++ api/routes/post.route.js
@@ -4,13 +4,14 @@ import {
   create,
   getposts,
   deletepost,
+  updatepost,
 } from "../controllers/post.controller.js";
 
 const router = express.Router();
 
 router.post("/create", verifyToken, create);
 router.get("/getposts", getposts);
-router.delete('/deletepost/:postId/:userId', verifyToken, deletepost)
-
+router.delete("/deletepost/:postId/:userId", verifyToken, deletepost);
+router.put("/updatepost/:postId/:userId", verifyToken, updatepost);
 
 export default router;
--- blog-app/src/App.jsx
+++ blog-app/src/App.jsx
@@ -11,6 +11,7 @@ import FooterComponent from "./components/Footer";
 import PrivateRoute from "./components/PrivateRoute";
 import AdminPrivateRoute from "./components/AdminPrivateRoute";
 import CreatePost from "./pages/CreatePost";
+import UpdatePost from "./pages/UpdatePost";
 
 export default function App() {
   return (
@@ -23,6 +24,7 @@ export default function App() {
         </Route>
         <Route element={<AdminPrivateRoute />}>
           <Route path="/create-post" element={<CreatePost />} />
+          <Route path="/update-post/:postId" element={<UpdatePost />} />
         </Route>
         <Route path="/projects" element={<Projects />}></Route>
         <Route path="/signin" element={<Signin />}></Route>
