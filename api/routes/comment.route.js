39e9fbc718e7d7abc5f5433432eb20c33bbef0fa
giahienpham <giahien14604@gmail.com>
2024-08-02 14:40:35 -0400

    add edit comment

--- api/routes/comment.route.js
+++ api/routes/comment.route.js
@@ -4,6 +4,7 @@ import {
   createComment,
   getPostComments,
   likeComment,
+  editComment,
 } from "../controllers/comment.controller.js";
 
 const router = express.Router();
@@ -11,5 +12,6 @@ const router = express.Router();
 router.post("/create", verifyToken, createComment);
 router.get("/getPostComments/:postId", getPostComments);
 router.put("/likeComment/:commentId", verifyToken, likeComment);
+router.put("/editComment/:commentId", verifyToken, editComment);
 
 export default router;
